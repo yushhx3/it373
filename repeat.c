1e60886267f22c63f68070c0ba76075e2fb4ff96
Aayushi Patel <aayushipatel009@gmail.com>
2017-05-04 06:39:28 +0000

    first commit

---
+++ repeat.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int num=123;
+    int sum=0;
+    
+    sum=sum+num;
+    sum=sum+num;
+    
+    printf("%d\n\r",sum);
+    
+    return 0;
+}
---
+++ repeat.s
@@ -0,0 +1,36 @@
+	.file	"repeat.c"
+	.section	.rodata
+.LC0:
+	.string	"%d\n\r"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$123, -8(%rbp)
+	movl	$0, -4(%rbp)
+	movl	-8(%rbp), %eax
+	addl	%eax, -4(%rbp)
+	movl	-8(%rbp), %eax
+	addl	%eax, -4(%rbp)
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	movl	$.LC0, %edi
+	movl	$0, %eax
+	call	printf
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 4.8.4-2ubuntu1~14.04.3) 4.8.4"
+	.section	.note.GNU-stack,"",@progbits
